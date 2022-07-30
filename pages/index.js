ae52a513e11850fb602997cca4d65f170e09b540
Carvalho <guilhermeclcosta@hotmail.com>
2022-07-30 00:51:42 -0300

    Adding components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import Image from 'next/Image'
 import Banner from '../components/banner/Banner'
+import Card from '../components/card/Card'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,6 +17,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <Banner buttonText="View stores nearby!" handleOnClick={handleOnBannerBtnClick}/>
+        <div className={styles.heroImage}>
+          <Image src="/static/hero-image.png" width={700} height={400}/>
+        </div>
+        <Card name="DarkHorse Coffee" imgUrl="/static/hero-image.png" href="/coffee-store/darkhorse-coffee"/>
       </main>
     </div>
   )
